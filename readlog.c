d11d209669c73b96ccc4c0afe4869d34f9e35d43
henryhe <changtianbonbon@gmai.com>
2012-07-16 18:01:32 +0800

    update readlog

--- readlog.c
+++ readlog.c
@@ -1,6 +1,30 @@
-#include <stdio.h>
-#include <time.h>
-#include <malloc.h>
+/**
+*author hepan
+*用于接受标准输入的日志，然后得到提交的数据，解析为各行都是有效数据的格式，格式如下
+*基站（wifi）号  x坐标  y坐标 pt  时间
+*/
+#include "locate.h"  //同目录下，包含了所有需要的头文件和声明
+
+
+//仅仅过滤掉不是提交数据的日志
+BOOL isNeededRawLine(char* line){
+  char* indicator = line;
+  while( *indicator != '\0'){
+    if(*indicator == '_' && *(indicator+1)=='x' && *(indicator+2)== '='){
+      return TRUE; 
+    }
+    indicator++;
+  }
+  return FALSE;
+}
+
+
+void parseLine(char * line){
+
+
+
+}
+
 void main(int argc,char * argv[]){
    time_t rawtime_start;
    struct tm* timeinfo;
@@ -8,16 +32,16 @@ void main(int argc,char * argv[]){
    timeinfo = localtime(&rawtime_start);
    printf("read log start:%s",asctime(timeinfo));
    long int line_num = 0;
-   char *line = (char *) malloc(1000);
-   while(fgets(line,1024,stdin)!=NULL){
+   char *line = (char *) malloc(READ_BUFFER_SIZE);
+   while(fgets(line,READ_BUFFER_SIZE,stdin)!=NULL){
         line_num++;
-	printf("%s",line);
+        if(isNeededRawLine(line) == TRUE)
+	        parseLine(line);
    }   
    time_t rawtime_end;
    time(&rawtime_end);
    timeinfo = localtime(&rawtime_end);
-   printf("read log end:%s",asctime(timeinfo));
+   printf("read log   end:%s",asctime(timeinfo));
+   printf("read lines : %ld\n",line_num);
 
-   printf("test for git pull");   
-   printf("test for git pull from local net");
 }
